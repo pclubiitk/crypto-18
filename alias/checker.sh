4c9e3309b238867b8ee1ee8aaf466ee2c3a35d46
Jenil Mewada <jenil@iitk.ac.in>
2018-08-31 23:41:38 +0530

    server.py->server2.py

--- alias/checker.sh
+++ alias/checker.sh
@@ -2,5 +2,5 @@
 str=$(ps -aux | grep Thanos| grep -v grep)
 
 if [ -z "$str" ];then
-	echo "The flag is : Thanos_will_return" > /home/jenil/Desktop/.pandora/.crypto/alias/flag.txt
+	chmod 777 /code/flag.txt
 fi
